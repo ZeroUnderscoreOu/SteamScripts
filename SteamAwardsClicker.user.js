f107847cb3a2987498ce1689165af88f9a5526dd
ZeroUnderscoreOu <ZeroUnderscoreOu@Yandex.ru>
2017-12-22 03:35:34 +0300

    Clicking awards per request

---
+++ SteamAwardsClicker.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Steam Awards Clicker
+// @author      ZeroUnderscoreOu
+// @version     1.0.1
+// @icon        
+// @description Clicker for a random Steam Awards vote
+// @namespace   https://github.com/ZeroUnderscoreOu/
+// @match       http://store.steampowered.com/SteamAwards/
+// @run-at      document-idle
+// @grant       none
+// ==/UserScript==
+
+let Nominations = document.querySelectorAll("Div.vote_nomination Div.btn_vote");
+let Vote = Math.floor(Math.random()*Nominations.length);
+Nominations[Vote].click();
