fa620ddb2e5fba612aaf489569d414cb22d050b9
Sergei <ZeroUnderscoreOu@users.noreply.github.com>
2022-12-24 08:21:34 +0300

    Couple of fixes, including for being unable to read page variables

--- SteamQueueSpinner.user.js
+++ SteamQueueSpinner.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        Steam Queue Spinner
 // @author      ZeroUnderscoreOu
-// @version     1.3.2
+// @version     1.3.3
 // @icon        
 // @description Spinner for your Steam Discovery Queue
 // @namespace   https://github.com/ZeroUnderscoreOu/
 // @match       *://store.steampowered.com/explore*
+// @run-at      document-idle
 // @grant       none
 // ==/UserScript==
 
@@ -21,22 +22,43 @@ var Queues = document.querySelector("Div.discover_queue_empty Div.subtext"); // 
 var Style = document.createElement("Style");
 var Button = document.createElement("Div");
 var Div = document.querySelector("Div.discovery_queue_customize_ctn");
+var RGAD = [];
 
-Queues = Queues ? parseInt(Queues.textContent.match(/-?\d/)[0],10) : 1; // defaulting to 1 as it seems to be the standard now
+// unsafeWindow variables; don't know any better way to read page's variables
+var GSID = unsafeWindow.GStoreItemData;
+var GSession = unsafeWindow.g_sessionID;
+
+console.log("UW",Object.keys(unsafeWindow.GStoreItemData.rgAppData));
+
+try {
+	Queues = parseInt(Queues.textContent.match(/-?\d/)[0],10);
+} catch (Data) {
+	console.error("SQS -",Data);
+	Queues = 1; // defaulting to 1 as it seems to be the standard now
+};
 /*
 if (Queues<1) { // potential fix for Steam error
 	Queues = 3;
 };
 */
+
 console.log("SQS - queues expected:",Queues);
 Style.textContent = "#QueueButton {Min-Width: 100px; Text-Align: Center;}";
 Button.id = "QueueButton";
 Button.className = "btnv6_blue_hoverfade btn_medium";
-if (Object.keys(GStoreItemData.rgAppData).length==0) { // if page queue is empty
+
+try {
+	RGAD = Object.keys(GSID.rgAppData);
+} catch (Data) {
+	console.error("SQS -",Data);
+};
+
+if (RGAD.length==0) { // if page queue is empty
 	Button.addEventListener("click",QueueGenerate);
 } else {
 	Button.addEventListener("click",QueueGet);
 };
+
 Button = Button.appendChild(document.createElement("Span"))
 Button.textContent = "Spin";
 document.head.appendChild(Style);
@@ -47,7 +69,7 @@ function QueueGet(Event,Queue) { // intentionally not providing default value fo
 	// Store itself manages it fine now, but spinning fails;
 	// queue should contain full list, but IDK how to get it from page itself,
 	// so I use rgAppData as a fallback
-	var Ids = Queue || Object.keys(GStoreItemData.rgAppData);
+	var Ids = Queue || RGAD;
 	Button.textContent = `Spin (${Queues*12})`; // for visibility with empty queues
 	console.log("SQS -",Queues,Ids.join(", "));
 	Queues--;
@@ -62,20 +84,22 @@ function QueueGenerate() {
 		body: Data,
 		credentials: "same-origin"
 	};
-	Data.set("sessionid",g_sessionID);
+	Data.set("sessionid",GSession);
 	Data.set("queuetype",0);
 	fetch(Address,Init)
 	.then((Data)=>(Data.json()))
 	.then((Data)=>{
-		GStoreItemData.rgAppData = Data.rgAppData; // writing for fallback
-		if (GStoreItemData.rgAppData==undefined) { // IDK why it's happening; need testing
+		if (Data.rgAppData==undefined) { // IDK why it's happening; need testing
 			console.log("SQS - bad response:",Data.rgAppData,Data.queue,"\r\n",Data);
-			GStoreItemData.rgAppData = {};
+			Data.rgAppData = {};
 		};
+		RGAD = Object.keys(Data.rgAppData); // writing for fallback
 		console.log("SQS - queue generated");
 		QueueGet(null,Data.queue);
 	})
-	.catch((Error)=>{console.error("SQS -",Error);});
+	.catch((Data)=>{
+		console.error("SQS -",Data);
+	});
 };
 
 function QueueClear(Ids) {
@@ -97,8 +121,10 @@ function QueueClear(Ids) {
 		body: Data,
 		credentials: "same-origin"
 	};
-	Data.set("sessionid",g_sessionID);
+	Data.set("sessionid",GSession);
 	Data.set("appid_to_clear_from_queue",Id);
-	fetch(Address,Init).catch((Error)=>{console.error("SQS -",Error);});
+	fetch(Address,Init).catch((Data)=>{
+		console.error("SQS -",Data);
+	});
 	Button.textContent = `Spin (${Queues*12+Ids.length})`;
 };
